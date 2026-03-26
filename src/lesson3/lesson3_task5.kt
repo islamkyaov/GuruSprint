b480d359ab5c6251a3a0fd050f74f4259e537737
Ислам Кяов <islamkyaov607@gmail.com>
2026-03-26 09:35:21 +0300

    GS-3-5

---
+++ src/lesson3/lesson3_task5.kt
@@ -0,0 +1,15 @@
+package lesson3
+fun main() {
+    for (i in 1..10) {
+        if (i == 5) {
+            println("Пропускаем число 5")
+            continue
+        }
+        if (i == 8) {
+            println("Встретили число 8")
+            break
+        }
+        println(i)
+    }
+    println("Цикл прерван")
+}
